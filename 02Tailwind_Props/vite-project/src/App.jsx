c10f90d6f75fcfe358d59bb4bf8c4a0189213e6e
Ayush sen <senayush920@gmail.com>
2025-02-03 14:13:27 +0530

    Maintain cards using props

--- 02Tailwind_Props/vite-project/src/App.jsx
+++ 02Tailwind_Props/vite-project/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p class ="text-8xl">vite</p>
+      <p class ="text-4xl bg-green-400 text-black p-4 mb-4">Tailwind test</p>
+      <Card name="Bda Mogembo" tittle="S-Mongo"/>
+      <Card name="Mogembo" tittle ="T-Mongo"/>
+
     </>
   )
 }
